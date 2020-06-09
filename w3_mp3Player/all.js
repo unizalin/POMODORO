d1d6a9e804d18da977ca8e887cc2d512a70fea07
Unizalin <uniza.lin@heroit.com.tw>
2020-06-09 18:20:56 +0800

    try jplayer

---
+++ w3_mp3Player/all.js
@@ -0,0 +1,11 @@
+// // AIzaSyDE5fuDj77_WaWjxpv3p4mG2omY5eMNUB0
+// let xml = new XMLHttpRequest()
+// xml.addEventListener('load',reqListener)
+// xml.open('GET','https://www.googleapis.com/youtube/v3/playlistItems?snippet,contentDetails,status&playlistId=RDOIMZk0rUcuM&key=AIzaSyDE5fuDj77_WaWjxpv3p4mG2omY5eMNUB0')
+// xml.send()
+// function reqListener(){
+//   console.log('res',this.response)
+// }
+
+const musicList = new Array()
+let test = document.getElementById('test')
